cf6a0b78c96ae282e67dd49ee7bb88eea90b873c
saikiran9704478510 <poleboinasaikiran123@gmail.com>
2025-08-26 11:57:11 +0530

    Convert CucumberBDDProject from submodule to regular files

---
+++ CucumberBDDProject/src/test/java/runner/TestRunner2.java
@@ -0,0 +1,19 @@
+package runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+    features = "src/test/resources/Features/logpom.feature",
+    glue = {"stepDefinitions"},
+    plugin = {
+        "pretty",
+        "html:target/cucumber-reports/cucumber.html",
+        "json:target/cucumber-reports/cucumber.json"
+        // add Extent adapter later if you want:
+        // "tech.grasshopper.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+    },
+    monochrome = true
+)
+public class TestRunner2 extends AbstractTestNGCucumberTests {
+}
